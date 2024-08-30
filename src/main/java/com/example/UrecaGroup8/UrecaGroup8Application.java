8a09bf9f08a26ac73dc4c356316ddf70eee800ae
hh830 <jhh8890@naver.com>
2024-08-30 09:34:12 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/UrecaGroup8/UrecaGroup8Application.java
@@ -0,0 +1,13 @@
+package com.example.UrecaGroup8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrecaGroup8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrecaGroup8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UrecaGroup8/UrecaGroup8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UrecaGroup8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrecaGroup8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
