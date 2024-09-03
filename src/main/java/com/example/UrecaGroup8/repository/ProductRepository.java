e0cb5b59a5b296914cad8baa7ec2f926e32dcee8
E.star <128211688+Estar0622@users.noreply.github.com>
2024-09-03 17:29:24 +0900

    Revert "Donggue"

--- src/main/java/com/example/UrecaGroup8/repository/ProductRepository.java
+++ src/main/java/com/example/UrecaGroup8/repository/ProductRepository.java
@@ -3,5 +3,5 @@ package com.example.UrecaGroup8.repository;
 import com.example.UrecaGroup8.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product,Long> {
 }
